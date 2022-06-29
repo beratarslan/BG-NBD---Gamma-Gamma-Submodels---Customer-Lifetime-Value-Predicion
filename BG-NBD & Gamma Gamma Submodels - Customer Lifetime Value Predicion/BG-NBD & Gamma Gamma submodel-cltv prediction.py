2fcbfffda3ef971339778751b7ff6941830bc1f2
Berat Arslan <55740614+beratarslan@users.noreply.github.com>
2022-06-29 08:37:07 +0300

    Customer Lifetime Value Prediction
    CLTV prediction with BG-NBD & Gamma Gamma Submodels

---
+++ BG-NBD & Gamma Gamma Submodels - Customer Lifetime Value Predicion/BG-NBD & Gamma Gamma submodel-cltv prediction.py
@@ -0,0 +1,231 @@
+#########################################
+# Customer Lifetime Value Prediction
+##########################################
+
+#https://archive.ics.uci.edu/ml/datasets/Online+Retail+II
+
+#Data Set Information:
+
+#This Online Retail II data set contains all the transactions occurring for a UK-based and registered,
+# non-store online retail between 01/12/2009 and 09/12/2011.The company mainly sells unique all-occasion gift-ware.
+# Many customers of the company are wholesalers.
+
+#Attribute Information:
+
+#InvoiceNo: Invoice number. Nominal. A 6-digit integral number uniquely assigned to each transaction.
+# If this code starts with the letter 'c', it indicates a cancellation.
+#StockCode: Product (item) code. Nominal. A 5-digit integral number uniquely assigned to each distinct product.
+#Description: Product (item) name. Nominal.
+#Quantity: The quantities of each product (item) per transaction. Numeric.
+#InvoiceDate: Invice date and time. Numeric. The day and time when a transaction was generated.
+#UnitPrice: Unit price. Numeric. Product price per unit in sterling (Â£).
+#CustomerID: Customer number. Nominal. A 5-digit integral number uniquely assigned to each customer.
+#Country: Country name. Nominal. The name of the country where a customer resides.
+
+##############################################################
+# 1.Data Preperation
+##############################################################
+
+# !pip install lifetimes
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+from lifetimes.plotting import plot_period_transactions
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.float_format', lambda x: '%.4f' % x)
+from sklearn.preprocessing import MinMaxScaler
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    # dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+#########################
+# Reading the Dataset
+#########################
+
+df_ = pd.read_excel(r"C:\Users\Berat Arslan\PycharmProjects\pythonProject1\DSMLBC8\materials\moduls\crm_analytics\datasets\online_retail_II.xlsx", sheet_name="Year 2009-2010")
+df = df_.copy()
+df.describe().T
+df.head()
+df.isnull().sum()
+
+#########################
+# Data Preprocessing
+#########################
+
+df.dropna(inplace=True)
+df = df[~df["Invoice"].str.contains("C", na=False)]
+df = df[df["Quantity"] > 0]
+df = df[df["Price"] > 0]
+
+replace_with_thresholds(df, "Quantity")
+replace_with_thresholds(df, "Price")
+
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+
+today_date = dt.datetime(2011, 12, 11)
+
+#########################
+# Preperation of Lifetime Data Structure
+#########################
+
+# recency: Weekly time elapsed over the last purchase
+# T: Customer's Age. Weekly.
+# frequency: total number of repeat purchases (frequency>1)
+# monetary: average earnings per purchase
+
+cltv_df = df.groupby('Customer ID').agg(
+    {'InvoiceDate': [lambda InvoiceDate: (InvoiceDate.max() - InvoiceDate.min()).days,
+                     lambda InvoiceDate: (today_date - InvoiceDate.min()).days],
+     'Invoice': lambda Invoice: Invoice.nunique(),
+     'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+cltv_df.columns = cltv_df.columns.droplevel(0)
+
+cltv_df.columns = ['recency', 'T', 'frequency', 'monetary']
+
+cltv_df["monetary"] = cltv_df["monetary"] / cltv_df["frequency"]
+
+cltv_df.describe().T
+
+cltv_df = cltv_df[(cltv_df['frequency'] > 1)]
+
+cltv_df["recency"] = cltv_df["recency"] / 7
+
+cltv_df["T"] = cltv_df["T"] / 7
+
+##############################################################
+# 2. BG-NBD Model
+##############################################################
+
+bgf = BetaGeoFitter(penalizer_coef=0.001)
+
+bgf.fit(cltv_df['frequency'],
+        cltv_df['recency'],
+        cltv_df['T'])
+
+################################################################
+# Who are the 10 customers we expect the most purchases from in 1 week?
+################################################################
+
+bgf.conditional_expected_number_of_purchases_up_to_time(1,
+                                                        cltv_df['frequency'],
+                                                        cltv_df['recency'],
+                                                        cltv_df['T']).sort_values(ascending=False).head(10)
+
+bgf.predict(1,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sort_values(ascending=False).head(10)
+
+cltv_df["expected_purc_1_week"] = bgf.predict(1,
+                                              cltv_df['frequency'],
+                                              cltv_df['recency'],
+                                              cltv_df['T'])
+
+################################################################
+# Who are the 10 customers we expect the most to purchase in 1 month?
+################################################################
+
+bgf.predict(4,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sort_values(ascending=False).head(10)
+
+cltv_df["expected_purc_1_month"] = bgf.predict(4,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency'],
+                                               cltv_df['T'])
+
+bgf.predict(4,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sum()
+
+################################################################
+# What is the Expected Sales Number of the Whole Company in 3 Months?
+################################################################
+
+bgf.predict(4 * 3,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sum()
+
+cltv_df["expected_purc_3_month"] = bgf.predict(4 * 3,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency'],
+                                               cltv_df['T'])
+
+################################################################
+# Evaluation of Forecast Results
+################################################################
+
+plot_period_transactions(bgf)
+plt.show()
+
+##############################################################
+# 3. GAMMA-GAMMA Model
+##############################################################
+
+ggf = GammaGammaFitter(penalizer_coef=0.01)
+
+ggf.fit(cltv_df['frequency'], cltv_df['monetary'])
+
+ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                        cltv_df['monetary']).head(10)
+
+ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                        cltv_df['monetary']).sort_values(ascending=False).head(10)
+
+cltv_df["expected_average_profit"] = ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                                                             cltv_df['monetary'])
+cltv_df.sort_values("expected_average_profit", ascending=False).head(10)
+
+##############################################################
+# 4. Customer Lifetime Value Calculation by Using BG-NBD & Gamma Gamma Submodels
+##############################################################
+
+cltv = ggf.customer_lifetime_value(bgf,
+                                   cltv_df['frequency'],
+                                   cltv_df['recency'],
+                                   cltv_df['T'],
+                                   cltv_df['monetary'],
+                                   time=3,
+                                   freq="W",
+                                   discount_rate=0.01)
+
+cltv.head()
+
+cltv = cltv.reset_index()
+
+cltv_final = cltv_df.merge(cltv, on="Customer ID", how="left")
+cltv_final.sort_values(by="clv", ascending=False).head(10)
+
+##############################################################
+# Creating CLTV Segments
+##############################################################
+
+cltv_final
+
+cltv_final["segment"] = pd.qcut(cltv_final["clv"], 4, labels=["D", "C", "B", "A"])
+
+cltv_final.sort_values(by="clv", ascending=False).head(50)
+
+cltv_final.groupby("segment").agg(
+    {"count", "mean", "sum"})
+
+cltv_final2.to_csv("cltv_prediction.csv")
